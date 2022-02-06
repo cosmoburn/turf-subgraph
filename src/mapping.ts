4beb8c971b53123eb1f6351a8dd8cd7c7bef6e54
Arthur Silveira <arthur.saboya@gmail.com>
2022-02-06 12:57:24 -0700

    initial commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,82 +1,28 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  Turf,
-  Approval,
-  ApprovalForAll,
-  OwnershipTransferred,
-  Transfer
-} from "../generated/Turf/Turf"
-import { ExampleEntity } from "../generated/schema"
+  Transfer as TransferEvent,
+  Turf as TokenContract,
+} from "../generated/Turf/Turf";
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+import { Token, User } from "../generated/schema";
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+export function handleTransfer(event: TransferEvent): void {
+  let token = Token.load(event.params.tokenId.toString());
+  if (!token) {
+    token = new Token(event.params.tokenId.toString());
+    token.creator = event.params.to.toHexString();
+    token.tokenID = event.params.tokenId;
+    token.createdAtTimestamp = event.block.timestamp;
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    let tokenContract = TokenContract.bind(event.address);
+    token.contentURI = tokenContract.tokenURI(event.params.tokenId);
+    token.image = `https://turf-assets.s3.us-west-1.amazonaws.com/plots/${event.params.tokenId}.png`;
   }
+  token.owner = event.params.to.toHexString();
+  token.save();
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.baseMysteryZoneTokenURI(...)
-  // - contract.baseTokenURI(...)
-  // - contract.baseTokenURILocked(...)
-  // - contract.currentMode(...)
-  // - contract.founderPassClaimed(...)
-  // - contract.getApproved(...)
-  // - contract.getWalletAddresses(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.maxSupply(...)
-  // - contract.mysteryZoneBaseURILocked(...)
-  // - contract.mysteryZones(...)
-  // - contract.name(...)
-  // - contract.nextOwnerToExplicitlySet(...)
-  // - contract.openSeaProxyRegistryAddress(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.price(...)
-  // - contract.royaltyInfo(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
-  // - contract.verifyPresale(...)
+  let user = User.load(event.params.to.toHexString());
+  if (!user) {
+    user = new User(event.params.to.toHexString());
+    user.save();
+  }
 }
-
-export function handleApprovalForAll(event: ApprovalForAll): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
-
-export function handleTransfer(event: Transfer): void {}
